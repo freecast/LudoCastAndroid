1d51a35d7644dfd18735100933cc70eaaabd6a3d
rongbaoqi <rongbaoqi@gmail.com>
2014-09-24 15:52:47 +0800

    change gamerule to viewerpage

--- src/com/example/casthelloworld/GameRule.java
+++ src/com/example/casthelloworld/GameRule.java
@@ -1,93 +1,169 @@
 package com.example.casthelloworld;
 
-import java.lang.reflect.Field;
-
-import org.json.JSONException;
-
-import com.google.sample.castcompanionlibrary.cast.VideoCastManager;
-import com.google.sample.castcompanionlibrary.cast.callbacks.VideoCastConsumerImpl;
-import com.google.sample.castcompanionlibrary.cast.exceptions.NoConnectionException;
-import com.google.sample.castcompanionlibrary.cast.exceptions.TransientNetworkDisconnectionException;
+import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.media.MediaRouter.RouteInfo;
-import android.text.method.ScrollingMovementMethod;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewConfiguration;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
 
-public class GameRule extends ActionBarActivity{
-	
-    private static final String TAG = "GameRule";
+/**
+ * Android实现左右滑动指引效果
+ * @Description: Android实现左右滑动指引效果
 
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.game_rule);
-		Log.d(TAG, "onCreate() was called");
-		TextView textView = (TextView)findViewById(R.id.GameRule_Text);   
-		textView.setMovementMethod(ScrollingMovementMethod.getInstance());
+ * @File: MyGuideViewActivity.java
 
-		DisplayMetrics mDisplayMetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(mDisplayMetrics);
-		int W = mDisplayMetrics.widthPixels;
-		int H = mDisplayMetrics.heightPixels;
-		Log.i(TAG, "Width = " + W);
-		Log.i(TAG, "Height = " + H);
-		if(H == 1280)
-		textView.setMaxLines(22);
-		
-		if(H < 1280)
-		textView.setMaxLines(17);
+ * @Package com.test.guide
 
-		if(H > 1280)
-		textView.setMaxLines(24);
+ * @Author Hanyonglu
 
-	}
-	
+ * @Date 2012-4-6 下午11:15:18
+
+ * @Version V1.0
+ */
+public class GameRule extends Activity {
+	 private ViewPager viewPager;  
+	 private ArrayList<View> pageViews;  
+	 private ImageView imageView;  
+	 private ImageView[] imageViews; 
+	 // 包裹滑动图片LinearLayout
+	 private ViewGroup main;
+	 // 包裹小圆点的LinearLayout
+	 private ViewGroup group;
+	    
+    /** Called when the activity is first created. */
     @Override
-	protected void onResume() {
-		Log.d(TAG, "onResume() was called");
-	
-		super.onResume();
-	}
-	
-	@Override
-	protected void onStop() {
-		Log.d(TAG, "onStop() was called");
-		super.onStop();
-	}
-
-
-	@Override
-	protected void onPause() {
-		Log.d(TAG, "onPause() was called");
-		super.onPause();
-	}
-
-	
-
-	@Override
-	protected void onDestroy() {
-		Log.d(TAG, "PlayGame onDestroy() is called");
-		super.onDestroy();
-	}
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 设置无标题窗口
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        LayoutInflater inflater = getLayoutInflater();  
+        pageViews = new ArrayList<View>();  
+        pageViews.add(inflater.inflate(R.layout.game_rule_item1, null));
+        pageViews.add(inflater.inflate(R.layout.game_rule_item2, null));
+        pageViews.add(inflater.inflate(R.layout.game_rule_item3, null));  
+        pageViews.add(inflater.inflate(R.layout.game_rule_item4, null));  
+        pageViews.add(inflater.inflate(R.layout.game_rule_item5, null));  
+        pageViews.add(inflater.inflate(R.layout.game_rule_item6, null));  
+        
+        imageViews = new ImageView[pageViews.size()];  
+        main = (ViewGroup)inflater.inflate(R.layout.game_rule, null);  
+        
+        group = (ViewGroup)main.findViewById(R.id.viewGroup1);  
+        viewPager = (ViewPager)main.findViewById(R.id.guidePages1);  
+        
+        for (int i = 0; i < pageViews.size(); i++) {  
+            imageView = new ImageView(GameRule.this);  
+            imageView.setLayoutParams(new LayoutParams(20,20));  
+            imageView.setPadding(20, 0, 20, 0);  
+            imageViews[i] = imageView;  
+            
+            if (i == 0) {  
+                //默认选中第一张图片
+                imageViews[i].setBackgroundResource(R.drawable.page_indicator_focused);  
+            } else {  
+                imageViews[i].setBackgroundResource(R.drawable.page_indicator);  
+            }  
+            
+            group.addView(imageViews[i]);  
+        }  
+        
+        setContentView(main);
+        
+        viewPager.setAdapter(new GuidePageAdapter());  
+        viewPager.setOnPageChangeListener(new GuidePageChangeListener());  
+    }
+    
+    // 指引页面数据适配器
+    class GuidePageAdapter extends PagerAdapter {  
+  	  
+        @Override  
+        public int getCount() {  
+            return pageViews.size();  
+        }  
+  
+        @Override  
+        public boolean isViewFromObject(View arg0, Object arg1) {  
+            return arg0 == arg1;  
+        }  
+  
+        @Override  
+        public int getItemPosition(Object object) {  
+            // TODO Auto-generated method stub  
+            return super.getItemPosition(object);  
+        }  
+  
+        @Override  
+        public void destroyItem(View arg0, int arg1, Object arg2) {  
+            // TODO Auto-generated method stub  
+            ((ViewPager) arg0).removeView(pageViews.get(arg1));  
+        }  
+  
+        @Override  
+        public Object instantiateItem(View arg0, int arg1) {  
+            // TODO Auto-generated method stub  
+            ((ViewPager) arg0).addView(pageViews.get(arg1));  
+            return pageViews.get(arg1);  
+        }  
+  
+        @Override  
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {  
+            // TODO Auto-generated method stub  
+  
+        }  
+  
+        @Override  
+        public Parcelable saveState() {  
+            // TODO Auto-generated method stub  
+            return null;  
+        }  
+  
+        @Override  
+        public void startUpdate(View arg0) {  
+            // TODO Auto-generated method stub  
+  
+        }  
+  
+        @Override  
+        public void finishUpdate(View arg0) {  
+            // TODO Auto-generated method stub  
+  
+        }  
+    } 
+    
+    // 指引页面更改事件监听器
+    class GuidePageChangeListener implements OnPageChangeListener {  
+    	  
+        @Override  
+        public void onPageScrollStateChanged(int arg0) {  
+            // TODO Auto-generated method stub  
+  
+        }  
+  
+        @Override  
+        public void onPageScrolled(int arg0, float arg1, int arg2) {  
+            // TODO Auto-generated method stub  
+  
+        }  
+  
+        @Override  
+        public void onPageSelected(int arg0) {  
+            for (int i = 0; i < imageViews.length; i++) {  
+                imageViews[arg0].setBackgroundResource(R.drawable.page_indicator_focused);
+                
+                if (arg0 != i) {  
+                    imageViews[i].setBackgroundResource(R.drawable.page_indicator);  
+                }  
+            }
+        }  
+    }  
 }
